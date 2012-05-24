b7270f161054d677b36f68b20ab92a6a0f2eaaaf
Zaiste! λ <oh@zaiste.net>
2012-05-24 15:27:19 +0200

    entire ring included

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [postgresql/postgresql "9.1-901-1.jdbc4"]
                  [org.clojure/java.jdbc "0.2.0"]
+                 [ring "1.1.0"]
                  [ring/ring-jetty-adapter "1.1.0"]
                  [compojure "1.0.2"]
                  [hiccup "1.0.0"]]
