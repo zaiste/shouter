5ece574d82ab367ed343f17453164101dd83497a
Zaiste! λ <oh@zaiste.net>
2012-05-24 15:28:16 +0200

    table generated with index

--- src/shouter/views/shouts.clj
+++ src/shouter/views/shouts.clj
@@ -1,20 +1,25 @@
 (ns shouter.views.shouts 
   (:use [hiccup.core :only [html h]]
         [hiccup.page :only [doctype]]
-        [hiccup.form :only [form-to label text-area submit-button]])
+        [hiccup.form :only [form-to label text-field text-area submit-button]])
   (:require [shouter.views.layout :as layout]))
 
 (defn shout-form []
-  [:div {:id "shout-form" :class "some class"}
-   (form-to [:post "/"]
+  [:div {:id "shout-form" :class "well"}
+   (form-to {:class "form"} [:post "/new"] 
             (label "shout" "What do you want to shout?")
-            (text-area "shout")
-            (submit-button "SHOUT"))])
+            (text-area {:class "input-xlarge" :rows 1} "shout")
+            (submit-button {:class "btn" } " ADD "))])
 
 (defn display-shouts [shouts]
-  [:div {:id "shouts"}
+  [:table {:id "shouts" :class "table table-striped"}
    (map 
-     (fn [shout] [:h2 {:class "shout" } (h (:body shout))])
+     (fn [idx shout] 
+       [:tr {:class "shout" } 
+        [:td idx]
+        [:td (h (:body shout))]
+        [:td (:created_at shout)]])
+     (iterate inc 0)
      shouts)])
 
 (defn index [shouts]
